58344428300d059cc5052bcc80a38035a07370b5
Prabgun Mokha <prabgunmokha@gmail.com>
2026-04-25 22:12:50 +0530

    Delete directory

--- clean-stadium-project/src/app/api/recommend/route.js
+++
@@ -1,7 +0,0 @@
-import { getRecommendations } from '@/lib/state';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  const recommendations = getRecommendations();
-  return NextResponse.json(recommendations);
-}
--- clean-stadium-project/src/app/api/state/route.js
+++
@@ -1,6 +0,0 @@
-import { simulationState } from '@/lib/state';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  return NextResponse.json(simulationState);
-}
